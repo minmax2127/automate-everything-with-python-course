e9f0265caed20a5735fa687b862b2d438e6f6a01
minmax2127 <mbernales2127@gmail.com>
2025-08-11 11:53:00 +0800

    Finished: Web Scraping with Automated Login exercise

---
+++ s2-web_scraping/login-click-scrape.py
@@ -0,0 +1,85 @@
+"""
+Web Scraper with Automated Login
+--------------------------------
+Logs into a webste and scrapes static and dynamic text from the homepage
+"""
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import os
+import time
+
+# Configuration
+CHROME_DRIVER_PATH = "C:\\Users\\Maxine\\Documents\\courses\\automate-everything-with-python-course\\s2-web_scraping\\chromedriver.exe"
+WEB_URL = "http://automated.pythonanywhere.com/login/"
+
+# Demo Credentials (stored in environment variables or fallback for demo)
+USERNAME = os.getenv("WEB_SCRAPER_USER", "automated")
+PASSWORD = os.getenv("WEB_SCRAPER_PASSWORD", "automatedautomated")
+
+CHROME_ARGUMENTS = [
+    "disable-infobars", 
+    "start-maximized", 
+    "disable-dev-shm-usage", 
+    "no-sandbox", 
+    "disable-blink-features=AutomationControlled"
+]
+
+def get_driver(url):
+    """ Returns the driver with custom options and navigate to the web url """
+    options = webdriver.ChromeOptions()
+    # add arguments to the options
+
+    for arg in CHROME_ARGUMENTS:
+        options.add_argument(arg)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+    # create driver and configure its settings
+    service = Service(CHROME_DRIVER_PATH)
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get(url)
+    return driver
+
+def wait_for_element(driver, by, value, timeout = 10):
+    """ Wait for the element to be present on the page then return it """
+    return WebDriverWait(driver, timeout).until(
+        EC.presence_of_element_located((by, value))
+    )
+
+def get_element_text(driver, by, value):
+    """ Returns the extracted text from a website tag """
+    element = wait_for_element(driver, by, value)
+    return element.text
+
+def scrape_homepage(driver):
+    """ Scrapes the Homepage for the static and dynamic value"""
+    time.sleep(2)
+
+    static_text = get_element_text(driver, By.XPATH, "/html/body/div[1]/div/h1[1]")
+    dynamic_text = get_element_text(driver, By.XPATH, "/html/body/div[1]/div/h1[2]")
+
+    # filter dynamic text
+    dynamic_text = dynamic_text.split(': ', 1)[1]
+
+
+    return static_text, dynamic_text
+
+if __name__ == "__main__":
+    driver = get_driver(WEB_URL)
+
+    # Login
+    username_input = wait_for_element(driver, By.ID, "id_username").send_keys(USERNAME)
+    password_input = wait_for_element(driver, By.ID, "id_password").send_keys(PASSWORD + Keys.RETURN)
+    
+    # Navigate to homepage
+    home_button = wait_for_element(driver, By.XPATH,  "/html/body/nav/div/a").click()
+    
+    # scrape the homepage
+    static, dynamic = scrape_homepage(driver)
+    print("Static text: ", static)
+    print("Dynamic text: ", dynamic)
